50a50ea0e2dd62032e45e381ab8de5123fd2f058
pranathi-2000 <pranathikarumudi1@gmail.com>
2025-03-12 15:42:51 -0500

    module1.py

--- module1.py
+++ module1.py
@@ -1,3 +1,8 @@
+#Create a greeting for your program.
+#Ask the user for the city that they grew up in and store it in a variable.
+#Ask the user for the name of a pet and store it in a variable.
+#Combine the name of their city and pet and show them their band name.
+
 print("Welcome to the Band Name Generator.")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
